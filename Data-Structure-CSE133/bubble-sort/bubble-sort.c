d93291f958d16f4bd25dd917a0be368de9e8e1b7
mdyasinahmed <yasinahmed7875@gmail.com>
2023-01-21 12:20:51 +0600

    Bubble Sort code added

---
+++ Data-Structure-CSE133/bubble-sort/bubble-sort.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+int main()
+{
+    int size, step, store, i;
+    scanf("%d", &size);
+    int a[size];
+    printf("Enter all values: ");
+    for (step = 0; step < size; step++)
+    {
+       scanf("%d", &a[step]);
+    }
+    
+    for(step = 0; step < size; step++)
+    {
+        for(i=0; i < size-step-1; i++)
+        {
+            if(a[i]>a[i+1])
+            {
+                store = a[i];
+                a[i] = a[i+1];
+                a[i+1] = store;
+            }
+        }
+    }
+    printf("\nYour Values in Ascending Order: ");
+    for (step = 0; step < size; step++)
+    {
+       printf("%d ", a[step]);
+    }
+
+    for(step = 0; step < size; step++)
+    {
+        for(i=0; i < size-step-1; i++)
+        {
+            if(a[i]<a[i+1])
+            {
+                store = a[i];
+                a[i] = a[i+1];
+                a[i+1] = store;
+            }
+        }
+    }
+    printf("\n");
+    printf("\nYour Values in Descending Order: ");
+    for (step = 0; step < size; step++)
+    {
+       printf("%d ", a[step]);
+    }
+
+    return 0;
+}
