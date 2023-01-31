4fc530a8aa85e106fc6ab5a0114a840d0f5df569
mdyasinahmed <yasinahmed7875@gmail.com>
2023-01-31 22:18:55 +0600

    selection sort code added

---
+++ DS Practice/selection-sort/selection-sort.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#define s 100
+int main()
+{
+    int n, arr[s], index, i;
+    printf("Enter Number of Element: ");
+    scanf("%d", &n);
+    
+    for(i=0;i<n;i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    for(i=0;i<n;i++)
+    {
+        if(a[i]>a[i+1])
+        {
+            /* code */
+        }
+        
+    }
+}
