72bbff98c37aac058eae749a9f9f48e2b4981d78
mdyasinahmed <yasinahmed7875@gmail.com>
2023-05-02 21:56:24 +0600

    Lab_Assignment-2: Bubble-Sort code added

---
+++ Sorting - Data Structure/Quick-Sort.c/Bubble-Sort.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+// Function for Swap 2 item Address
+void swap(int *x, int *y)
+{
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+// Function for Bubble-Sort Operation
+void bubbleSort(int a[], int size)
+{
+    int step, store, i;
+    for(step = 0; step < size; step++)
+    {
+        for(i=0; i < size-step-1; i++)
+        {
+            if(a[i]>a[i+1])
+            {
+                swap(&a[i], &a[i+1]);
+            }
+        }
+    }
+}
+
+// Function for Printing Array
+void printArray(int array[], int arraySize)
+{
+    int step;
+    for (step = 0; step < arraySize; step++)
+    {
+       printf("%d ", array[step]);
+    }
+    printf("\n");
+}
+
+// Main Function
+int main()
+{
+    int s, arr[s];
+    scanf("%d", &s);
+
+    for(int i=0; i<s; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    
+    bubbleSort(arr,s);
+    printArray(arr,s);
+    
+    return 0;
+}
