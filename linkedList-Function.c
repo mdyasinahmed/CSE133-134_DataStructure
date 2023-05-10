20a0bcccac6c957283df2f4aa8a291a7a7fd9aad
mdyasinahmed <yasinahmed7875@gmail.com>
2023-05-10 12:56:01 +0600

    linkedList with function code updated

--- linkedList-Function.c
+++ linkedList-Function.c
@@ -26,46 +26,57 @@ struct node* insertAtBeginning(struct node* head, int value) {
 // insert at the end
 struct node* insertAtEnd(struct node* head, int value) {
     struct node* newNode = createNode(value);
-    if (head == NULL) {
+    if (head == NULL)
+    {
         head = newNode;
         return head;
     }
     struct node* temp = head;
-    while (temp->next != NULL) {
+    while (temp->next != NULL)
+    {
         temp = temp->next;
     }
+
     temp->next = newNode;
     return head;
 }
 
 // delete node
-struct node* deleteNode(struct node* head, int value) {
-    if (head == NULL) {
+struct node* deleteNode(struct node* head, int value)
+{
+    if(head == NULL)
+    {
         return head;
     }
-    if (head->value == value) {
+    if(head->value == value)
+    {
         struct node* temp = head;
         head = head->next;
         free(temp);
         return head;
     }
     struct node* temp = head;
-    while (temp->next != NULL && temp->next->value != value) {
+    while(temp->next != NULL && temp->next->value != value)
+    {
         temp = temp->next;
     }
-    if (temp->next == NULL) {
+    if(temp->next == NULL)
+    {
         return head;
     }
+
     struct node* deleteNode = temp->next;
     temp->next = temp->next->next;
     free(deleteNode);
+    
     return head;
 }
 
 // free the memory
 void freeList(struct node* head) {
     struct node* temp;
-    while (head != NULL) {
+    while (head != NULL)
+    {
         temp = head;
         head = head->next;
         free(temp);
@@ -74,32 +85,35 @@ void freeList(struct node* head) {
 
 // print the list
 void printList(struct node* head) {
-    while (head != NULL) {
+    while (head != NULL)
+    {
         printf("%d ", head->value);
         head = head->next;
     }
     printf("\n");
 }
 
-// Main function
-int main() {
+// main
+int main()
+{
     struct node* head = NULL;
     head = insertAtEnd(head, 5);
     head = insertAtEnd(head, 8);
     head = insertAtEnd(head, 11);
     head = insertAtEnd(head, 15);
     head = insertAtEnd(head, 18);
-    printf("Original list: ");
+    printf("List Main: ");
     printList(head);
 
     head = insertAtBeginning(head, 2);
-    printf("List after inserting 2 at the beginning: ");
+    printf("After Insert at BEG: ");
     printList(head);
 
     head = deleteNode(head, 11);
-    printf("List after deleting node with value 11: ");
+    printf("After Deleting 11: ");
     printList(head);
 
     freeList(head);
+
     return 0;
 }
