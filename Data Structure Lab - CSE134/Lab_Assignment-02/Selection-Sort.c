21b6ac1ca1c8abde17a8e4a1c7bbcee4a9ade578
mdyasinahmed <yasinahmed7875@gmail.com>
2023-05-13 09:28:07 +0600

    selection sort code included

--- Data Structure Lab - CSE134/Lab_Assignment-02/Selection-Sort.c
+++ Data Structure Lab - CSE134/Lab_Assignment-02/Selection-Sort.c
@@ -12,28 +12,33 @@ int main()
         scanf("%d", &a[i]);
     }
 
-    for(i=0;i<n;i++)
+    for(i=0;i<n-1;i++)
     {
         min = a[i];
-        for(j=1+i;j<n;j++)
+        position = i;
+        for(j=i+1;j<n;j++)
         {
-            if(min>a[j])
+            if(a[j] < min)
             {
                 min = a[j];
+                position = j;
             }
+        }
+
+        if(position != i)
+        {
+            int temp = a[i];
+            a[i] = a[position];
+            a[position] = temp;
         }
     }
 
-    if(a[i]>min)
-    {
-        a[position] = a[i];
-        a[i] = min;
-    }
-
     for(i=0;i<n;i++)
     {
-        printf("%d", a[i]);
+        printf("%d ", a[i]);
     }
 
     return 0;
 }
+
+
