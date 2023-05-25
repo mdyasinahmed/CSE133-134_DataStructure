ef6de4a5b2bd5b57c9df62ed2868eb6be2e69930
mdyasinahmed <yasinahmed7875@gmail.com>
2023-05-25 09:58:28 +0600

    binary search unsolved code is added

--- seaarching-binarySearch.c
+++ seaarching-binarySearch.c
@@ -1,12 +1,50 @@
 #include <stdio.h>
 #define s 1000
 
+// Function to Swap 2 value Address
+void swap(int *x, int *y)
+{
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
 
+// Function for Bubble Sort Operations
+void bubbleSort(int a[], int n){
+    int i, j;
 
+    for(i=0; i<n-1; ++i){
+        for(j=0; j<n-i-1; ++j){
+            if(a[j]>a[j+1]){
+                swap(&a[j], &a[j+1]);
+            }
+        }
+    }
+}
+// Function for binary search operation
+void binarySearch(int a[], int n, int x){
+    int i;
+    bubbleSort(a[],n);
+    
+    for(i=0; i<n; i++){
+        int beg = i, end = n-1, mid = (beg+mid)/2;
+        while(beg<=end)
+        {
+            if(a[mid] > x){
+                end = mid - 1;
+                mid = mid = (beg+mid)/2;
+            }
+            else{
+                beg = mid + 1;
+                mid = mid = (beg+mid)/2;
+            }
 
-
-
-
+            if(a[mid] == x){
+                printf("Value Matched\n");
+            }
+        }
+    }
+}
 int main()
 {
     int n, a[s];
@@ -15,6 +53,11 @@ int main()
     for(int i=0; i<n; i++){
         scanf("%d", &a[i]);
     }
+
+    int x;
+    scanf("%d", &x);
+
+    binarySearch(a, x);
 
 
 }
