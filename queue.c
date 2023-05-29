1e2b567c9ff11023d40f31dfee5b77a92e6fd1c1
mdyasinahmed <yasinahmed7875@gmail.com>
2023-05-29 09:19:43 +0600

    queue-isFull or not function added

--- queue.c
+++ queue.c
@@ -2,10 +2,26 @@
 #include <stdbool.h>
 #define Capacity 5
 
-int Queue[Capacity];
-int front = 0, rear = 0, item = 0;
+int queue[Capacity];
+int front = 0, rear = 0, totalItem = 0;
 
+bool isFull(){
+    if(totalItem == Capacity){
+        return true;
+    }
+    return false;
+}
 
+void enque(int item){
+    if(isFull){
+        printf("Queue is Full.\n");
+    }
+}
+
+int dequeue(){
+
+    return 0;
+}
 
 int main()
 {
