09b7440e6b762e29c00e7f7a2b2de728efedf612
mdyasinahmed <yasinahmed7875@gmail.com>
2023-05-29 08:55:18 +0600

    stack push code implemented

--- stack.c
+++ stack.c
@@ -1,8 +1,25 @@
 #include<stdio.h>
+#define Capacity 5
+int stack[Capacity];
+int top = -1;
+
+int push(int x){
+    if(top < Capacity-1){
+        top = top+1;
+        stack[top] = x;
+        printf("Added Item: %d\n", x);
+    }
+    else{
+        printf("No Spaces!\n");
+    }
+}
+
+
 
 int main()
 {
+    push(5);
 
-
+    printf("Top Value: %d\n", peak());
     return 0;
 }
