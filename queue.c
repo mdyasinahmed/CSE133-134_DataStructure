a9d230959bf58c84104f695013d2950cd27d0192
mdyasinahmed <yasinahmed7875@gmail.com>
2023-05-30 07:07:06 +0600

    queue practice code added

--- queue.c
+++ queue.c
@@ -12,19 +12,40 @@ bool isFull(){
     return false;
 }
 
-void enque(int item){
-    if(isFull){
-        printf("Queue is Full.\n");
+bool isEmpty(){
+    if(totalItem == 0){
+        return true;
     }
+    return false;
+}
+
+void enqeue(int item){
+    if(isFull()){
+        printf("Queue is Full.\n");
+        return;
+    }
+    rear = (rear+1)%Capacity;
+    queue[rear] = item;
+    totalItem++;
 }
 
 int dequeue(){
-
-    return 0;
+    if(isEmpty){
+        printf("Noting to Remove\n");
+    }
+    int frontItem = queue[front];
+    queue[front] = -1;
+    front = (front+1) % Capacity;
+    totalItem++;
+    return frontItem;
 }
 
 int main()
 {
+    enqeue(10);
+    enqeue(15);
+    enqeue(20);
+    dequeue();
 
     return 0;
 }
